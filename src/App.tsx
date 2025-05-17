29b13ffabf462547fa094a5b09bfe1848df898fb
Ryan Carvalho Bernardo <106412618+Ryaannsz@users.noreply.github.com>
2025-05-17 15:28:15 -0300

    fix: redirect auth page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import AuthPage from './pages/AuthPage'
 import ContactsPage from './pages/ContactsPage'
 import ProtectedRoute from './components/protectedRoute/ProtectedRoute'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import { isAuthenticated } from './utils/auth';
 
 function App() {
-
   return (
-    <>
+    <Routes>
+      {/* Redirecionamento da raiz */}
+      <Route
+        path="/"
+        element={
+          isAuthenticated() ? <Navigate to="/contatos" replace /> : <Navigate to="/auth" replace />
+        }
+      />
 
+      {/* Página pública */}
+      <Route path="/auth" element={<AuthPage />} />
 
-
-      <Routes>
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/contatos" element={
+      {/* Página protegida */}
+      <Route
+        path="/contatos"
+        element={
           <ProtectedRoute>
             <div className="flex flex-col min-h-screen">
               <Header />
@@ -25,11 +34,10 @@ function App() {
               <Footer />
             </div>
           </ProtectedRoute>
-        } />
-      </Routes>
-
-    </>
-  )
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App
