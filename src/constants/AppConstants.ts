164f469aadbe4263ef793cfc4960a1053a1b9a16
Ryan Carvalho Bernardo <106412618+Ryaannsz@users.noreply.github.com>
2025-05-18 20:11:27 -0300

    fix: constants

--- src/constants/AppConstants.ts
+++ src/constants/AppConstants.ts
@@ -1,7 +1,7 @@
 
 
-//const API_URL = "https://rotas-express-postgre-kq8m.vercel.app";
+const API_URL = "https://rotas-express-postgre-kq8m.vercel.app";
 
-const API_URL = "http://localhost:8080"
+//const API_URL = "http://localhost:8080"
 
 export default API_URL;
